9e3eb8cd0248016f7482311c9a23da4afa91aef4
anthony <tuffant21@aim.com>
2020-06-27 21:12:30 -0500

    Showing an autoinit example in the component.ts

--- example-project/src/app/components/auto-init/auto-init.component.ts
+++ example-project/src/app/components/auto-init/auto-init.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Angular2MaterializeV1Service} from 'angular2-materialize-v1';
 
 @Component({
   selector: 'app-auto-init',
   templateUrl: './auto-init.component.html',
   styleUrls: ['./auto-init.component.css']
 })
-export class AutoInitComponent implements OnInit {
+export class AutoInitComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private angular2MaterializeService: Angular2MaterializeV1Service) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.angular2MaterializeService.autoInit();
   }
 
 }
